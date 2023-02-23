fe66b697efafab186967e0653caf99c6fbcf5b4a
Mukles <Mukleshossen1763@gmail.com>
2023-02-23 10:03:44 +0600

    refactor index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoute = require("./routes/user");
 const gptRoute = require("./routes/chatGpt");
 const app = express();
 const corsOptions = {
-  origin: clientUrl,
+  origin: [clientUrl, "http://localhost:3000"],
   methods: "GET,POST,PUT,DELETE, PATCH",
   credentials: true,
 };
